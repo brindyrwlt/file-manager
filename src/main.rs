3f6718212b79ba2b9159740669afe18622258e9b
Brindy <brindyrwlt@proton.me>
2026-02-20 21:38:55 +0100

    grid for files is now working and started error messages

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
-use std::fs::read_dir;
+use std::fs::{read_dir, ReadDir};
 use std::rc::Rc;
 use slint::{ModelRc, SharedString, VecModel};
 
@@ -33,24 +33,31 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+// TODO: Handle error outside of this function (return error if not found, result if found and return files to set it outside of function
 fn get_files(ui: &AppWindow) -> Result<(), Box<dyn Error>> {
-    let files_in_dir = read_dir(ui.get_directory()).expect("Can't read into directory");
-    // println!("{:?}", files.enumerate());
+    let files_in_dir;
+    match read_dir(ui.get_directory()) {
+        Ok(dir) => {
+            files_in_dir = dir;
 
-    let mut files: Vec<FileItem> = vec![];
-    for file in files_in_dir.enumerate() {
-        // println!("{:?}", file.1?.file_name());
-        files.push(FileItem {
-            name: SharedString::from(file.1?.file_name().into_string().unwrap()),
-            size: 0.0,
-            fileType: FileItemType::FILE,
-        });
-    //     SharedString::from(file.1?.file_name().into_string().unwrap()), 0, FileItemType::FILE
+            let mut files: Vec<FileItem> = vec![];
+            for file in files_in_dir.enumerate() {
+                // println!("{:?}", file.1?.file_name());
+                files.push(FileItem {
+                    name: SharedString::from(file.1?.file_name().into_string().unwrap()),
+                    size: 0.0,
+                    fileType: FileItemType::FILE,
+                });
+                //     SharedString::from(file.1?.file_name().into_string().unwrap()), 0, FileItemType::FILE
+            }
+
+            let files_vec: Rc<VecModel<FileItem>> = Rc::new(VecModel::from(files));
+            let files= ModelRc::from(files_vec);
+
+            ui.set_files(files);
+            Ok(())
+        },
+        Err(e) => Err(e)
     }
-
-    let files_vec: Rc<VecModel<FileItem>> = Rc::new(VecModel::from(files));
-    let files= ModelRc::from(files_vec);
-
-    ui.set_files(files);
-    Ok(())
+    // println!("{:?}", files.enumerate());
 }

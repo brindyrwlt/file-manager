324e4cbc33b010362d6aa2bea95187f4e4624f1a
Brindy <brindyrwlt@proton.me>
2026-03-07 17:03:49 +0100

    error message working, TODO: make ok button close dialog

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
-use std::fs::{read_dir, ReadDir};
+use std::fs::{read_dir};
 use std::rc::Rc;
-use slint::{ModelRc, SharedString, VecModel};
+use slint::{ModelRc, SharedString, ToSharedString, VecModel};
 
 slint::include_modules!();
 
@@ -23,8 +23,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     ui.on_change_current_directory({
         let ui_handle = ui.as_weak();
+
         move || {
-            get_files(&ui_handle.unwrap());
+            let ui = ui_handle.unwrap();
+            match get_files(&ui_handle.unwrap()) {
+                Ok(files) => ui.set_files(files),
+                Err(err) => {
+                    let error_dialog = ErrorDialog::new().unwrap();
+                    error_dialog.set_error_text(err.to_shared_string());
+                    error_dialog.show().unwrap();
+                },
+            }
         }
     });
 
@@ -34,7 +43,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 // TODO: Handle error outside of this function (return error if not found, result if found and return files to set it outside of function
-fn get_files(ui: &AppWindow) -> Result<(), Box<dyn Error>> {
+fn get_files(ui: &AppWindow) -> Result<ModelRc<FileItem>, Box<dyn Error>> {
     let files_in_dir;
     match read_dir(ui.get_directory()) {
         Ok(dir) => {
@@ -48,16 +57,13 @@ fn get_files(ui: &AppWindow) -> Result<(), Box<dyn Error>> {
                     size: 0.0,
                     fileType: FileItemType::FILE,
                 });
-                //     SharedString::from(file.1?.file_name().into_string().unwrap()), 0, FileItemType::FILE
             }
 
             let files_vec: Rc<VecModel<FileItem>> = Rc::new(VecModel::from(files));
             let files= ModelRc::from(files_vec);
 
-            ui.set_files(files);
-            Ok(())
+            Ok(files)
         },
-        Err(e) => Err(e)
+        Err(e) => Err(Box::from(e))
     }
-    // println!("{:?}", files.enumerate());
 }
